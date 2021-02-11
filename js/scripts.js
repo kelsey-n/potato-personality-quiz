ed4cf13cd26258000658f26e9f89bf9bb77e4fe9
kelsey-n <kan317@nyu.edu>
2021-02-11 11:20:44 -0500

    changed if to switch; added comments

--- js/scripts.js
+++ js/scripts.js
@@ -6,33 +6,82 @@ var result=""
 //storing the selection in the 'result' variable
 $(".answer").click(function() {
 
-	if($(this).attr('id')=='answer-1-1'){result+="1";
-  $("#question-1").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
-  $("#question-2").show(800)}
-  if($(this).attr('id')=='answer-1-2'){result+="2";
-  $("#question-1").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
-  $("#question-2").show(800)}
-  if($(this).attr('id')=='answer-2-1'){result+="1";
-  $("#question-2").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
-  $("#question-3").show(800)}
-  if($(this).attr('id')=='answer-2-2'){result+="2";
-  $("#question-2").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
-  $("#question-3").show(800)}
-  if($(this).attr('id')=='answer-3-1'){result+="1";
-  $("#question-3").hide();
-  $("h2").hide();
-  $("h3").hide()}
-  if($(this).attr('id')=='answer-3-2'){result+="2";
-  $("#question-3").hide();
-  $("h2").hide();
-  $("h3").hide()}
+	//Switch function evaluates the attribute id of the specific answer clicked on
+	//and executes the case matching the answer id
+	//Each case appends the answer value (1 for first selection, 2 for second selection) to the result variable so end result can be assessed
+	//Each case also hides the current question and shows the next one
+	switch($(this).attr('id')) {
+		case 'answer-1-1':
+		result+="1";
+	  $("#question-1").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+	  $("#question-2").show(800);
+		break;
 
-  if(result=="111"){$("#result-3").show("slide", {direction: 'up'}, 2000)}
-  if(result=="112"){$("#result-5").show("slide", {direction: 'up'}, 2000)}
-  if(result=="121"){$("#result-7").show("slide", {direction: 'up'}, 2000)}
-  if(result=="122"){$("#result-1").show("slide", {direction: 'up'}, 2000)}
-  if(result=="211"){$("#result-2").show("slide", {direction: 'up'}, 2000)}
-  if(result=="212"){$("#result-4").show("slide", {direction: 'up'}, 2000)}
-  if(result=="221"){$("#result-8").show("slide", {direction: 'up'}, 2000)}
-  if(result=="222"){$("#result-6").show("slide", {direction: 'up'}, 2000)}
-  })
+		case 'answer-1-2':
+		result+="2";
+	  $("#question-1").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+	  $("#question-2").show(800);
+		break;
+
+		case 'answer-2-1':
+		result+="1";
+	  $("#question-2").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+	  $("#question-3").show(800);
+		break;
+
+		case 'answer-2-2':
+		result+="2";
+	  $("#question-2").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+	  $("#question-3").show(800);
+		break;
+
+		case 'answer-3-1':
+		result+="1";
+		$("#question-3").hide();
+	  $("h2").hide();
+	  $("h3").hide();
+		break;
+
+		case 'answer-3-2':
+		result+="2";
+		$("#question-3").hide();
+	  $("h2").hide();
+	  $("h3").hide();
+		break;
+	}
+
+	//This switch function shows the appropriate quiz result based on the user's result variable 
+	switch(result) {
+		case "111":
+		$("#result-3").show("slide", {direction: 'up'}, 2000);
+		break;
+
+		case "112":
+		$("#result-5").show("slide", {direction: 'up'}, 2000);
+		break;
+
+		case "121":
+		$("#result-7").show("slide", {direction: 'up'}, 2000);
+		break;
+
+		case "122":
+		$("#result-1").show("slide", {direction: 'up'}, 2000);
+		break;
+
+		case "211":
+		$("#result-2").show("slide", {direction: 'up'}, 2000);
+		break;
+
+		case "212":
+		$("#result-4").show("slide", {direction: 'up'}, 2000);
+		break;
+
+		case "221":
+		$("#result-8").show("slide", {direction: 'up'}, 2000);
+		break;
+
+		case "222":
+		$("#result-6").show("slide", {direction: 'up'}, 2000);
+		break;
+	}
+})
