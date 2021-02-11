72663c2ec52588fbc08ad8aeb20e49874621cd87
kelsey-n <kan317@nyu.edu>
2021-02-11 10:52:27 -0500

    added transitions in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,32 @@ var result=""
 $(".answer").click(function() {
 
 	if($(this).attr('id')=='answer-1-1'){result+="1";
-  $("#question-1").hide();
-  $("#question-2").show()}
+  $("#question-1").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+  $("#question-2").show(800)}
   if($(this).attr('id')=='answer-1-2'){result+="2";
-  $("#question-1").hide();
-  $("#question-2").show()}
+  $("#question-1").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+  $("#question-2").show(800)}
   if($(this).attr('id')=='answer-2-1'){result+="1";
-  $("#question-2").hide();
-  $("#question-3").show()}
+  $("#question-2").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+  $("#question-3").show(800)}
   if($(this).attr('id')=='answer-2-2'){result+="2";
-  $("#question-2").hide();
-  $("#question-3").show()}
+  $("#question-2").hide("scale", {percent: 0, direction: 'both', origin: ['middle','middle']}, 500);
+  $("#question-3").show(800)}
   if($(this).attr('id')=='answer-3-1'){result+="1";
   $("#question-3").hide();
+  $("h2").hide();
   $("h3").hide()}
   if($(this).attr('id')=='answer-3-2'){result+="2";
   $("#question-3").hide();
+  $("h2").hide();
   $("h3").hide()}
 
-  if(result=="111"){$("#result-3").show()}
-  if(result=="112"){$("#result-5").show()}
-  if(result=="121"){$("#result-7").show()}
-  if(result=="122"){$("#result-1").show()}
-  if(result=="211"){$("#result-2").show()}
-  if(result=="212"){$("#result-4").show()}
-  if(result=="221"){$("#result-8").show()}
-  if(result=="222"){$("#result-6").show()}
-  })  
+  if(result=="111"){$("#result-3").show("slide", {direction: 'up'}, 2000)}
+  if(result=="112"){$("#result-5").show("slide", {direction: 'up'}, 2000)}
+  if(result=="121"){$("#result-7").show("slide", {direction: 'up'}, 2000)}
+  if(result=="122"){$("#result-1").show("slide", {direction: 'up'}, 2000)}
+  if(result=="211"){$("#result-2").show("slide", {direction: 'up'}, 2000)}
+  if(result=="212"){$("#result-4").show("slide", {direction: 'up'}, 2000)}
+  if(result=="221"){$("#result-8").show("slide", {direction: 'up'}, 2000)}
+  if(result=="222"){$("#result-6").show("slide", {direction: 'up'}, 2000)}
+  })
